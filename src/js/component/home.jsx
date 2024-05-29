d7d0a04932c23f2d701000d2e1c641b91309637d
César Jovanni Zaldivar Camps <132796806+Cesarzaldivar@users.noreply.github.com>
2024-05-29 01:22:46 +0000

    mi todo list

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,24 +1,46 @@
-import React from "react";
+import React, { useState } from "react";
 
 //include images into your bundle
 import rigoImage from "../../img/rigo-baby.jpg";
 
 //create your first component
 const Home = () => {
+	const [inputValue, setInputValue] = useState('');
+	const [list, setList] = useState([]);
+
+	const addTask = (e) => {
+		if (e.key === "Enter") {
+			e.preventDefault()
+			setList([...list, inputValue]);
+			setInputValue('');
+		}
+	}
+
+	const deleteTask = (indexToRemove) => {
+		const newList = list.filter((_, index) => index !== indexToRemove);
+		setList(newList);
+	};
+	
 	return (
-		<div className="text-center">
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
+		<div className="container w-25 my-5">
+			<h1 className="text-center">To do list</h1>
+			<ul className="list-group">
+
+				<input className="list-group-item w-100"
+					type="text"
+					placeholder="Add a task"
+					onChange={event => setInputValue(event.target.value)}
+					value={inputValue}
+					onKeyDown={addTask}
+				/>
+				{list.map((item, index) => (
+					<li key={index} className="list-group-item d-flex justify-content-between align-items-center">
+						{item}
+						<i className="fas fa-trash" onClick={() => deleteTask(index)}></i>
+					</li>
+				))}
+			</ul>
+			<div>{list.length} item left</div>
 		</div>
 	);
 };
